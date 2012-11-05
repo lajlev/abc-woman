170fd07c3a432ced3422073ac342a963f8caeea5
jbe <jbe@trustpilot.com>
2012-11-05 08:56:29 +0100

    testing if index works on remote server

---
+++ FoosBall/Controllers/ConfigController.cs
@@ -0,0 +1,33 @@
+﻿namespace FoosBall.Controllers
+{
+    using System.Web.Mvc;
+
+    public class ConfigController : BaseController
+    {
+        public ActionResult Show()
+        {
+            var configCollection = Dbh.GetCollection<Models.Config>("Config").FindOne();
+
+            return View(configCollection);
+        }
+
+        [HttpPost]
+        public ActionResult Save(FormCollection form)
+        {
+            var configCollection = Dbh.GetCollection<Models.Config>("Config");
+            var config = configCollection.FindOne();
+
+            config.Name = form.GetValue("Name").AttemptedValue;
+            config.Version = form.GetValue("Version").AttemptedValue;
+            config.Domain = form.GetValue("Domain").AttemptedValue;
+            config.RequireDepartment = form.GetValue("RequireDepartment") != null;
+            config.RequireDomainValidation = form.GetValue("RequireDomainValidation") != null;
+            config.AllowOneOnOneMatches = form.GetValue("AllowOneOnOneMatches") != null;
+            config.GenderSpecificMatches = form.GetValue("GenderSpecificMatches") != null;
+
+            configCollection.Save(config);
+
+            return this.RedirectToAction("Show", "Config");
+        }
+    }
+}
---
+++ FoosBall/Models/Config.cs
@@ -0,0 +1,21 @@
+﻿namespace FoosBall.Models
+{
+    using FoosBall.Models.Base;
+
+    public class Config : FoosBallDoc
+    {
+        public string Name { get; set; }
+
+        public string Version { get; set; }
+
+        public string Domain { get; set; }
+
+        public bool RequireDepartment { get; set; }
+
+        public bool RequireDomainValidation { get; set; }
+
+        public bool AllowOneOnOneMatches { get; set; }
+
+        public bool GenderSpecificMatches { get; set; }
+    }
+}
