596b4d80aeddb82f26e8d945368749d0c4b559be
jbe <jbe@trustpilot.com>
2012-11-10 21:18:07 +0100

    added controller and view

---
+++ FoosBall/Controllers/StatsController.cs
@@ -0,0 +1,24 @@
+﻿
+namespace FoosBall.Controllers
+{
+    using System.Web.Mvc;
+
+    using FoosBall.Models;
+
+    using MongoDB.Driver.Builders;
+
+    public class StatsController : BaseController
+    {
+        public ActionResult Index(string playerId)
+        {
+            var playersCollection = Dbh.GetCollection<Player>("Players");
+            var matchCollection = Dbh.GetCollection<Match>("Matches");
+
+            var player = playersCollection.FindOne(Query.EQ("_id", playerId));
+
+
+            return View();
+        }
+
+    }
+}
