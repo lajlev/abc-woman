db7f4438a55d904ac389f5b4691611f31d2d7453
jbe <jbe@trustpilot.com>
2012-11-20 20:58:52 +0100

    minor stats stuff

---
+++ FoosBall/Models/AllPlayerStatsAggregate.cs
@@ -0,0 +1,23 @@
+﻿namespace FoosBall.Models
+{
+    public class AllPlayerStatsAggregate
+    {
+        public string MostFighting { get; set; }
+
+        public string MostWinning { get; set; }
+        
+        public string MostLosing { get; set; }
+        
+        public string TopRanked { get; set; }
+        
+        public string BottomRanked { get; set; }
+        
+        public string MostWinsInARow { get; set; }
+        
+        public string MostLossesInARow { get; set; }
+        
+        public string BiggestRatingWin { get; set; }
+        
+        public string PlayedToday { get; set; }
+    }
+}
