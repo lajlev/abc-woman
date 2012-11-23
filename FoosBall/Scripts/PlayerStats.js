6c0c4032f594315c64152e22a4c9891fe4a9aaac
jbe <jbe@trustpilot.com>
2012-11-23 18:32:45 +0100

    Sorted the Legendary matches table by GameOverTime. Put a tag on players that the match the current user so it's easier to see spot yourself in the match lists

--- FoosBall/Scripts/PlayerStats.js
+++ FoosBall/Scripts/PlayerStats.js
@@ -3,11 +3,13 @@
     /* ******************************************************************
      * Stats / Player View
      */
+    
+    // Render the Players rating chart
     var container = $("#player-rating-chart")[0];
     getChartData(container);
     
+    // Add a tag on the players that match the current user
     var currentUserId = $("#current-user-id").val();
-
     if (!!currentUserId === true) {
         $("." + currentUserId).append("<sup>(me)</sup>");
     }
