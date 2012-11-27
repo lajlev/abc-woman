5ba44892c8603a72107de6e4afaec062301e0133
jbe <jbe@trustpilot.com>
2012-11-27 17:22:31 +0100

    Moved the angular specific code into a separate js file. Corrected some jQuery Ajax setup.

---
+++ FoosBall/Scripts/_ngAdmin.js
@@ -0,0 +1,27 @@
+﻿function settingsCtrl($scope, $http) {
+    var dataStore = {},
+        inputs,
+        checkboxes;
+
+    $http.get("Admin/GetConfig").
+        success(function (data) {
+            dataStore.Settings = JSON.parse(data.Settings);
+            dataStore.Users = JSON.parse(data.Users);
+
+            inputs = [
+                { label: "Application name", value: dataStore.Settings.Name },
+                { label: "Domain name", value: dataStore.Settings.Domain },
+                { label: "Admin user email", value: dataStore.Settings.AdminAccount },
+            ];
+
+            checkboxes = [
+                { label: "Enable Departments", value: dataStore.Settings.RequireDepartment },
+                { label: "Enable Domain Validation", value: dataStore.Settings.RequireDomainValidation },
+                { label: "Allow one-on-one Matches", value: dataStore.Settings.AllowOneOnOneMatches },
+                { label: "Enable Gender Specific Matches", value: dataStore.Settings.GenderSpecificMatches },
+            ];
+
+            $scope.inputs = inputs;
+            $scope.checkboxes = checkboxes;
+        });
+}
