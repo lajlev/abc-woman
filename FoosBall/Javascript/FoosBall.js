19c8744ae7174cfe605a64dcd37e9e070219a5b9
jbe <jbe@trustpilot.com>
2013-08-15 09:55:09 +0200

    messing with Shared.js that apparaently can't be found anywhere. Trying again

--- FoosBall/Javascript/FoosBall.js
+++ FoosBall/Javascript/FoosBall.js
@@ -36,40 +36,6 @@
 
     // Start the connection.
     $.connection.hub.start();
-    
-    // ******************************************************************
-    // Menu highlighting and animation 
-    //
-    var activeTab = $('#page').attr('class');
-    var $menu = $('#main-menu');
-    var $menuarrow = $('.menu-arrow');
-
-    if (!!activeTab !== false) {
-        $menu.find('li').removeClass('selected');
-        $('.' + activeTab, $menu).addClass('selected');
-    }
-
-    $('.menu-list-button').on('click', function (e) {
-        e.preventDefault();
-        $menu.toggle();
-        $menuarrow.toggle();
-    });
-
-    $menu.on('click', 'li', function (event) {
-        event.preventDefault();
-        var redirectUrl;
-        var $target = $(event.target);
-
-        if ($target.is('li')) {
-            redirectUrl = $target.children('a').attr('href');
-        } else if ($target.is('a')) {
-            redirectUrl = $target.attr('href');
-        }
-
-        if (redirectUrl) {
-            window.location = redirectUrl;
-        }
-    });
 });
 
 /* ******************************************************************
