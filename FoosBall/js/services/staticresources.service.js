99d7c7c133954c9e29736a6d2f07a214ad1e7e22
jbe <jbe@trustpilot.com>
2014-03-31 21:31:52 +0200

    added data- in front of all ng- attributes

--- FoosBall/js/services/staticresources.service.js
+++ FoosBall/js/services/staticresources.service.js
@@ -1,23 +1,24 @@
 ﻿FoosBall.service('staticResources', [function () {
-    
+
     this.getBackgroundImageUrl = function ($scope) {
         var remoteUrl = 'https://s3-eu-west-1.amazonaws.com/images.trustpilot.com/static/foosball/background.jpg';
         var localUrl = '/css/foosball-background.jpg';
 
         if (!$scope.appSettings.Environment || $scope.appSettings.Environment === 'Local') {
-            return localUrl;
-        } else {
             return remoteUrl;
+        } else {
+            return localUrl;
         }
     };
+
     this.getFootballIconUrl = function ($scope) {
         var remoteUrl = 'https://s3-eu-west-1.amazonaws.com/images.trustpilot.com/static/foosball/icon_football.png';
         var localUrl = '/css/icon_football.png';
 
         if (!$scope.appSettings.Environment || $scope.appSettings.Environment === 'Local') {
-            return localUrl;
-        } else {
             return remoteUrl;
+        } else {
+            return localUrl;
         }
     };
 
@@ -26,9 +27,9 @@
         var localUrl = '/css/minified/foosball.css';
 
         if (!$scope.appSettings.Environment || $scope.appSettings.Environment === 'Local') {
-            return localUrl;
-        } else {
             return remoteUrl;
+        } else {
+            return localUrl;
         }
     };
 }]);
