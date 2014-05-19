b1996d7beefd982909a78ee3601c42e829a51e7b
j4kobbech <jbe@trustpilot.com>
2014-05-19 21:20:21 +0200

    Tools project for migration tasks

---
+++ FoosballTools/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace FoosballTools
+{
+    using System;
+    using FoosBall.Main;
+    using FoosBall.Models.Domain;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var dbConnection = new Db();
+            var playersCollection = dbConnection.Dbh.GetCollection<Player>("Players");
+            
+            dbConnection.Dbh.DropCollection("Users");
+            
+            var usersCollection = dbConnection.Dbh.GetCollection<User>("Users");
+
+            foreach (var player in playersCollection.FindAll())
+            {
+                var user = new User
+                {
+                    Name = player.Name,
+                    Email = player.Email,
+                    Password  = player.Password,
+                    Deactivated = player.Deactivated,
+                    RememberMe = player.RememberMe
+                };
+
+                Console.WriteLine(player.Name);
+                usersCollection.Save(user);
+            }
+        }
+    }
+}
